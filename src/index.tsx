0ffb14411c70dbf80d2e424903483324cd931c10
Catminusminus <getomya@svk.jp>
2021-11-20 19:45:36 +0900

    implemented useParse

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,24 @@
+import { useState, useCallback, Fragment } from "react";
 import ReactDOM from "react-dom";
+import { loadDefaultJapaneseParser } from "budoux";
 
-const App = () => <div>Hello!</div>;
+const useParse = () => {
+  const [parser, _] = useState(loadDefaultJapaneseParser());
+  const parse = useCallback((input: string) => parser.parse(input), [parser]);
+  return parse;
+};
+
+const sentense = "あなたに寄り添う最先端のテクノロジー。";
+
+const App = () => {
+  const parse = useParse();
+  const lines = parse(sentense).map((line, index) => (
+    <Fragment key={index}>
+      {line}
+      <br />
+    </Fragment>
+  ));
+  return <div>{lines}</div>;
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
